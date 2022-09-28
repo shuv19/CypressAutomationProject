0fa6dfb1d3e56c6c30b7024c4875d8311aee5115
shuv19 <shuv730@gmail.com>
2022-09-28 22:16:39 +0530

    Hello

--- cypress/integration/TestSuite_ShopMobile/TC1_ShopMobile_E2EPractice.js
+++ cypress/integration/TestSuite_ShopMobile/TC1_ShopMobile_E2EPractice.js
@@ -89,6 +89,6 @@ it('Purchase Two Mobile Phones in angularpractice Page', function()
     cy.log("End to End test case execution is completed.");
     cy.log("Testing Git");
     cy.log("Testing Git 3");
+    cy.log("Testing Git 3");
 } )
 } )
-//
